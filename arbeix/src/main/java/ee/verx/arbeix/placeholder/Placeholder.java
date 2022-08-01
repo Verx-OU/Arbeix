83e4260aca07b204957a806a3ac84cf63e67d28c
Emilia Dreamer <emilia@rymiel.space>
2022-08-01 18:08:31 +0300

    Fix mistakes in Placeholder enum

--- arbeix/src/main/java/ee/verx/arbeix/placeholder/Placeholder.java
+++ arbeix/src/main/java/ee/verx/arbeix/placeholder/Placeholder.java
@@ -3,14 +3,15 @@ package ee.verx.arbeix.placeholder;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("SpellCheckingInspection")
 public enum Placeholder {
   CL_NAME("KLIENT_NIMI", Type.TEXT),
   CL_COMPANY("KLIENT_ETTEVOTE", Type.TEXT),
-  CL_MAIL("KLIENT_TELEFON", Type.EMAIL),
-  CL_TEL("KLIENT_EKIRI", Type.TEXT),
+  CL_MAIL("KLIENT_TELEFON", Type.TEXT),
+  CL_TEL("KLIENT_EKIRI", Type.EMAIL),
   CL_OBJ("KLIENT_OBJEKT", Type.TEXT),
   CL_DATE("KUUPAEV", Type.DATE),
-  NR("HINNAPAKKUMINE_NR", Type.DATE);
+  NR("HINNAPAKKUMINE_NR", Type.TEXT);
 
   final @NotNull String localName;
   final @NotNull Type type;
--- arbeix/src/main/java/ee/verx/arbeix/placeholder/Replacement.java
+++ arbeix/src/main/java/ee/verx/arbeix/placeholder/Replacement.java
@@ -1,6 +1,5 @@
 package ee.verx.arbeix.placeholder;
 
-import org.intellij.lang.annotations.MagicConstant;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Calendar;
@@ -9,7 +8,7 @@ import java.util.GregorianCalendar;
 public sealed interface Replacement {
   record Text(String value) implements Replacement {}
   record Email(String value) implements Replacement {}
-  record Date(int year, @MagicConstant(intValues = {Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, Calendar.JUNE, Calendar.JULY, Calendar.AUGUST, Calendar.SEPTEMBER, Calendar.OCTOBER, Calendar.NOVEMBER, Calendar.DECEMBER}) int month, int day) implements Replacement {
+  record Date(int year, int month, int day) implements Replacement {
     public @NotNull Calendar toCalendar() {
       var date = GregorianCalendar.getInstance();
       date.set(year, month, day);
